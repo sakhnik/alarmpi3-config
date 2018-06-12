98c3d4e87589fd502f29553776dcbe6b20069c6a
Anatolii Sakhnik <sakhnik@gmail.com>
2018-06-12 10:29:45 +0300

    Start saving configuration of alarmpi3

---
+++ 70-apps.sh
@@ -0,0 +1,44 @@
+AddPackage autoconf # A GNU tool for automatically configuring source code
+AddPackage automake # A GNU tool for automatically creating Makefiles
+AddPackage binutils # A set of programs to assemble and manipulate binary and object files
+AddPackage bison # The GNU general-purpose parser generator
+AddPackage bzip2 # A high-quality data compression program
+AddPackage cmake # A cross-platform open-source make system
+AddPackage cower # A simple AUR agent with a pretentious name
+AddPackage crda # Central Regulatory Domain Agent for wireless networks
+AddPackage cryptsetup # Userspace setup tool for transparent encryption of block devices using dm-crypt
+AddPackage dialog # A tool to display dialog boxes from shell scripts
+AddPackage diffutils # Utility programs used for creating patch files
+AddPackage e2fsprogs # Ext2/3/4 filesystem utilities
+AddPackage ffmpeg # Complete solution to record, convert and stream audio and video
+AddPackage gawk # GNU version of awk
+AddPackage gcc # The GNU Compiler Collection - C and C++ frontends
+AddPackage gcc-libs # Runtime libraries shipped by GCC
+AddPackage gettext # GNU internationalization library
+AddPackage git # the fast distributed version control system
+AddPackage make # GNU make utility to maintain groups of programs
+AddPackage man-db # A utility for reading man pages
+AddPackage man-pages # Linux man pages
+AddPackage mc # Midnight Commander is a text based filemanager/shell that emulates Norton Commander
+AddPackage nginx # Lightweight HTTP server and IMAP/POP3 proxy server
+AddPackage ninja # Small build system with a focus on speed
+AddPackage openssh # Free version of the SSH connectivity tools
+AddPackage openvpn # An easy-to-use, robust and highly configurable VPN (Virtual Private Network)
+AddPackage patch # A utility to apply patch files to original sources
+AddPackage pciutils # PCI bus configuration space access library and tools
+AddPackage perl # A highly capable, feature-rich programming language
+AddPackage rsync # A file transfer program to keep remote files in sync
+AddPackage socat # Multipurpose relay
+AddPackage stow # Manage installation of multiple softwares in the same directory tree
+AddPackage sudo # Give certain users the ability to run some commands as root
+AddPackage tmux # A terminal multiplexer
+AddPackage usbutils # USB Device Utilities
+AddPackage vi # The original ex/vi text editor
+AddPackage vim # Vi Improved, a highly configurable, improved version of the vi text editor
+AddPackage wget # Network utility to retrieve files from the Web
+AddPackage which # A utility to show the full path of commands
+
+
+AddPackage --foreign kerberosio-machinery # An image processing framework, which uses your USB-, IP- or RPi-camera to recognize events (e.g. motion).
+AddPackage --foreign kerberosio-web # Kerberos.io machinery
+AddPackage --foreign php-mcrypt # mcrypt module for PHP
