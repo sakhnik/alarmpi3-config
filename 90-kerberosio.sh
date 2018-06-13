82a68784860fc95fe94e7421622cdd094d0bc632
Anatolii Sakhnik <sakhnik@gmail.com>
2018-06-13 23:09:31 +0300

    Clean up some kerberosio files

--- 90-kerberosio.sh
+++ 90-kerberosio.sh
@@ -4,13 +4,11 @@ AddPackage --foreign ffmpeg2.8 # Complete solution to record, convert and stream
 AddPackage --foreign kerberosio-machinery # An image processing framework, which uses your USB-, IP- or RPi-camera to recognize events (e.g. motion).
 AddPackage --foreign kerberosio-web # Kerberos.io machinery
 
-CopyFile /etc/opt/kerberosio/config/algorithm.xml
 CopyFile /etc/opt/kerberosio/config/capture.xml
 CopyFile /etc/opt/kerberosio/config/cloud.xml
 CopyFile /etc/opt/kerberosio/config/condition.xml
 CopyFile /etc/opt/kerberosio/config/config.xml
 CopyFile /etc/opt/kerberosio/config/expositor.xml
-CopyFile /etc/opt/kerberosio/config/heuristic.xml
 CopyFile /etc/opt/kerberosio/config/io.xml
 CopyFile /etc/opt/kerberosio/config/stream.xml
 CopyFile /etc/opt/kerberosio/scripts/email.sh 755 sakhnik users
