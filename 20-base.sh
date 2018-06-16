bad610489d816c4f38e1031ed006a66b3dde4552
Anatolii Sakhnik <sakhnik@gmail.com>
2018-06-16 06:36:45 +0300

    /etc/shells

--- 20-base.sh
+++ 20-base.sh
@@ -95,10 +95,15 @@ KERNEL=="mouse*|mice|event*",  MODE="0777"
 EOF
 
 CreateLink /etc/systemd/system/getty.target.wants/getty@tty1.service /usr/lib/systemd/system/getty@.service
+CreateLink /etc/systemd/system/multi-user.target.wants/haveged.service /usr/lib/systemd/system/haveged.service
+CreateLink /etc/systemd/system/multi-user.target.wants/remote-fs.target /usr/lib/systemd/system/remote-fs.target
+CreateLink /etc/systemd/user/sockets.target.wants/dirmngr.socket /usr/lib/systemd/user/dirmngr.socket
 CreateLink /etc/systemd/user/sockets.target.wants/gpg-agent-browser.socket /usr/lib/systemd/user/gpg-agent-browser.socket
 CreateLink /etc/systemd/user/sockets.target.wants/gpg-agent-extra.socket /usr/lib/systemd/user/gpg-agent-extra.socket
 CreateLink /etc/systemd/user/sockets.target.wants/gpg-agent-ssh.socket /usr/lib/systemd/user/gpg-agent-ssh.socket
 CreateLink /etc/systemd/user/sockets.target.wants/gpg-agent.socket /usr/lib/systemd/user/gpg-agent.socket
 
 
+IgnorePath '/etc/.updated'
 CopyFile /etc/fstab
+CopyFile /etc/shells
