ae82401ba9a5a57868567ba5c37b30fbdf7257e5
Anatolii Sakhnik <sakhnik@gmail.com>
2018-07-24 22:06:57 +0300

    Add tinc network farm

--- 50-net.sh
+++ 50-net.sh
@@ -33,9 +33,20 @@ AddPackage postfix # Fast, easy to administer, secure mail server
 AddPackage s-nail # Mail processing system with a command syntax reminiscent of ed
 CreateLink /etc/systemd/system/multi-user.target.wants/postfix.service /usr/lib/systemd/system/postfix.service
 
+AddPackage nfs-utils # Support programs for Network File Systems
+CreateLink /etc/systemd/system/multi-user.target.wants/nfs-server.service /usr/lib/systemd/system/nfs-server.service
+CreateDir /srv/nfs4/data
+cat >>"$(GetPackageOriginalFile nfs-utils /etc/exports)" <<EOF
+
+/srv/nfs4               *(ro,sync,fsid=0)
+/srv/nfs4/data          192.168.13.0/24(rw,sync,nohide)
+EOF
+
 
 AddPackage tinc # VPN (Virtual Private Network) daemon
 CreateLink /etc/systemd/system/multi-user.target.wants/tinc.service /usr/lib/systemd/system/tinc.service
+
+# Tinc network home
 CreateLink /etc/systemd/system/tinc.service.wants/tinc@home.service /usr/lib/systemd/system/tinc@.service
 
 cat >"$(CreateFile /etc/tinc/home/tinc.conf)" <<EOF
@@ -63,12 +74,29 @@ CopyFile /etc/tinc/home/hosts/potter
 DecryptFileTo /etc/tinc/home/rsa_key.priv.gpg /etc/tinc/home/rsa_key.priv
 SetFileProperty /etc/tinc/home/rsa_key.priv mode 600
 
+# Tinc network farm
+CreateLink /etc/systemd/system/tinc.service.wants/tinc@farm.service /usr/lib/systemd/system/tinc@.service
 
-AddPackage nfs-utils # Support programs for Network File Systems
-CreateLink /etc/systemd/system/multi-user.target.wants/nfs-server.service /usr/lib/systemd/system/nfs-server.service
-CreateDir /srv/nfs4/data
-cat >>"$(GetPackageOriginalFile nfs-utils /etc/exports)" <<EOF
-
-/srv/nfs4               *(ro,sync,fsid=0)
-/srv/nfs4/data          192.168.13.0/24(rw,sync,nohide)
+cat >"$(CreateFile /etc/tinc/farm/tinc.conf)" <<EOF
+Name = alarmpi3
+AddressFamily = ipv4
+Device = /dev/net/tun
 EOF
+
+cat >"$(CreateFile /etc/tinc/farm/tinc-up 755)" <<'EOF'
+#!/bin/sh
+
+ifconfig $INTERFACE 10.0.2.1 netmask 255.255.255.0
+EOF
+
+cat >"$(CreateFile /etc/tinc/farm/tinc-down 755)" <<'EOF'
+#!/bin/sh
+
+ifconfig $INTERFACE down
+EOF
+
+CopyFile /etc/tinc/farm/hosts/alarmpi3
+CopyFile /etc/tinc/farm/hosts/kionia
+CopyFile /etc/tinc/farm/hosts/pangea
+DecryptFileTo /etc/tinc/farm/rsa_key.priv.gpg /etc/tinc/farm/rsa_key.priv
+SetFileProperty /etc/tinc/farm/rsa_key.priv mode 600
